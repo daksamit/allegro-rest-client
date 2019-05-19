24f33908c1dd9a142ce4b4152c18d2d443ee0ce4
Dominik <daksamit@daksamit.pl>
2019-05-19 20:08:16 +0200

    add jest tests

--- test/test.spec.ts
+++ test/test.spec.ts
@@ -1,4 +1,3 @@
-import { expect } from "chai";
 import AllegroRestClient from "../src";
 
 const clientConfig = {
@@ -12,41 +11,13 @@ const clientConfig = {
 // https://allegro.pl.allegrosandbox.pl/auth/oauth/authorize?response_type=code&client_id=bcfaf324a2104824b81fbd97aea46654&redirect_uri=https://allegro-rest-client.daksamit.pl
 const allegroClient = new AllegroRestClient(clientConfig, { sandbox: true, account: "test_account" });
 
-describe("AllegroClient", () => {
-  it("should throw error invalid auth code", async () => {
-    try {
-      await allegroClient.authorize("wrong_auth_code");
-    } catch (err) {
-      expect(err).haveOwnProperty("error");
-      expect(err).haveOwnProperty("error_description");
-      expect(Object.keys(err).length).to.equal(2);
-    }
-  });
-  // it("should authorize app in 10 seconds", async () => {
-  //   try {
-  //     const tokens: any = await allegroClient.authorize("YbEJSW1FUMAxXFXAI6jH5bgyLw6Xb1OQ");
-  //     expect(tokens).haveOwnProperty("access_token");
-  //     expect(tokens).haveOwnProperty("refresh_token");
-  //     expect(tokens).haveOwnProperty("token_type");
-  //     expect(tokens).haveOwnProperty("expires_in");
-  //     expect(tokens).haveOwnProperty("scope");
-  //     expect(tokens).haveOwnProperty("jti");
-  //     expect(Object.keys(tokens).length).to.equal(6);
-  //   } catch (err) {
-  //     expect(err).to.not.throw(err);
-  //   }
-  // });
-  it("should refresh saved token", async () => {
-    try {
-      const tokens: any = await allegroClient.refreshTokens();
-      expect(tokens).haveOwnProperty("access_token");
-      expect(tokens).haveOwnProperty("refresh_token");
-      expect(tokens).haveOwnProperty("token_type");
-      expect(tokens).haveOwnProperty("expires_in");
-      expect(tokens).haveOwnProperty("jti");
-      expect(Object.keys(tokens).length).to.equal(5);
-    } catch (err) {
-      expect(err).to.not.throw(err);
-    }
-  });
-});
+test('authorize client, should throw error invalid auth code', async () => {
+  try {
+    await allegroClient.authorize("invalid_auth_code");
+  } catch (err) {
+    const keys = Object.keys(err)
+    expect(err).toHaveProperty("error");
+    expect(err).toHaveProperty("error_description");
+    expect(Object.keys(err).length).toEqual(2);
+  }
+})
