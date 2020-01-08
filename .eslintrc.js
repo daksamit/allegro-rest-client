0f9c2355922bea759c5bc84fd073112148a65cf9
Dominik Aksamit <dapsyduck@gmail.com>
2020-01-08 09:25:19 +0100

    update eslint/priettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,6 @@ module.exports =  {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/camelcase': 'off',
   },
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,7 @@
 module.exports =  {
-  semi:  true,
+  semi:  false,
   trailingComma:  'all',
   singleQuote:  true,
-  printWidth:  80,
+  printWidth:  120,
   tabWidth: 2,
 };
